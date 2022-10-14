5374d8c238916e9806eafbd48bf61cd4819ca964
William9923 <williamong1400@gmail.com>
2022-10-14 08:42:56 +0700

    feat: auto format on save

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -10,6 +10,16 @@ local formatting = null_ls.builtins.formatting
 -- Ref: https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+local lsp_formatting = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			-- apply whatever logic you want (in this example, we'll only use null-ls)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+
 null_ls.setup({
 	debug = false,
 	sources = {
@@ -44,20 +54,9 @@ null_ls.setup({
 				group = augroup,
 				buffer = bufnr,
 				callback = function()
-					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-					vim.lsp.buf.formatting_sync()
+					lsp_formatting(bufnr)
 				end,
 			})
 		end
 	end,
 })
-
-local lsp_formatting = function(bufnr)
-	vim.lsp.buf.format({
-		filter = function(client)
-			-- apply whatever logic you want (in this example, we'll only use null-ls)
-			return client.name == "null-ls"
-		end,
-		bufnr = bufnr,
-	})
-end
