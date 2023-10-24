26f010051f8ceef428bf8db364ab41b55e76acb0
William Ong <william.ong@gojek.com>
2023-10-24 10:21:42 +0700

    fix: treesitter keep complaining about deprecated method

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -78,4 +78,5 @@ if not status_ok then
 end
 
 local ts_utils = require("nvim-treesitter.ts_utils")
-ts_utils.get_node_text = vim.treesitter.query.get_node_text
+ts_utils.get_node_text = vim.treesitter.get_node_text
+ts_utils.is_in_node_range = vim.treesitter.is_in_node_range
